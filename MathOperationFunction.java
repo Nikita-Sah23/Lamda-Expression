e3cb343a3883cc684c4c0d4c9419a123b51c7f65
Nikita-sah <nikitasah4gmail.com>
2022-10-29 14:49:51 +0530

    sec commit

---
+++ MathOperationFunction.java
@@ -0,0 +1,24 @@
+package com.lamdaexpression;
+
+public class MathOperationFunction {
+	
+	interface IMathFunction{
+		int calculate(int a, int b);
+		static void printResult(int a,int b, String function,IMathFunction fobj) {
+			System.out.println("Result of "+function+" is "+fobj.calculate(a, b));
+		}
+	}
+	
+	public static void main(String args[]) {
+		IMathFunction add = (a,b) -> a+b;
+		IMathFunction sub = (a,b) -> a-b;
+		IMathFunction multiply = (a,b) -> a*b;
+		IMathFunction div = (a,b) -> a/b;
+		
+		IMathFunction.printResult(9,3,"Addition",add);
+		IMathFunction.printResult(9,3,"Substraction",sub);
+		IMathFunction.printResult(9,3,"Multiplication",multiply);
+		IMathFunction.printResult(9,3,"Division",div);
+	}
+	
+}
